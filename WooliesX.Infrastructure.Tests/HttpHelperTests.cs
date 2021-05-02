20a358ee44ac976003d7c3ac49036180ca20a571
Jithin Jayasankar <Jithin.Jayasankar@iriworldwide.com.au>
2021-05-02 16:50:26 +1000

    Added http helper tests

---
+++ WooliesX.Infrastructure.Tests/HttpHelperTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WooliesX.Infrastructure.Http;
+using Xunit;
+
+namespace WooliesX.Infrastructure.Tests
+{
+    public class HttpHelperTests
+    {
+        [Theory]
+        [InlineData("http://www.foo.com/", "/u1", "token")]
+        [InlineData("http://www.foo.com", "u1/", "token")]
+        public void GenerateUrl_ShouldReturnMergedUrl(string baseUrl, string endpoint, string token)
+        {
+            var expected = GenerateUrl(baseUrl, endpoint, token);
+            var actual = HttpHelper.GenerateUrl(baseUrl, endpoint, token);
+
+            Assert.Equal(expected, actual);
+        }
+
+        private string GenerateUrl(string baseUrl, string endpoint, string token)
+        {
+            if (baseUrl.EndsWith('/'))
+            {
+                baseUrl = baseUrl.Remove(baseUrl.LastIndexOf('/'));
+            }
+            if (endpoint.StartsWith('/'))
+            {
+                endpoint = endpoint.Remove(endpoint.IndexOf('/'));
+            }
+
+            return $"{baseUrl}/{endpoint}?token={token}";
+        }
+    }
+}
